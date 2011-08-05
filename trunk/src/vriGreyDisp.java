040bfd7af11117cc8e3f3bafadd136dfb4e2d219
small <small@e08a94be-91bf-a212-c12f-41944ddfc591>
2011-08-05 07:37:18 +0000

    Further refactoring: removed most of vriUtils.

--- trunk/src/vriGreyDisp.java
+++ trunk/src/vriGreyDisp.java
@@ -24,84 +24,9 @@ import java.util.Arrays;
 
 //####################################################################//
 
+enum PlotTypes {PHASE, REAL, IMAG, AMPL, COLOUR};
+
 class EmptyImageException extends Exception {};
-
-
-class FFTArray {
-    int imsize;
-    float data[];
-
-    public FFTArray(int i) {
-        imsize = i;
-        data = new float[2*imsize*imsize];
-    }
-
-    static FFTArray fromSquareArray(SquareArray sa) {
-        FFTArray result = new FFTArray(sa.size);
-        for (int i=0; i<sa.size; i++) {
-            for (int j=0; j<sa.size; j++) {
-                result.set(i, j, sa.get(i, j), 0);
-            }
-        }
-        return result;
-    }
-
-    public FFTArray(int i, float[] d) {
-        imsize = i;
-        data = d;
-    }
-
-    public void set(int i, int j, float real, float im) {
-        data[j*imsize*2 + i*2] = real;
-        data[j*imsize*2 + i*2 + 1] = im;
-    }
-
-    public float getReal(int i, int j) {
-        return data[j*imsize*2 + i*2];
-    }
-    
-    public float getImag(int i, int j) {
-        return data[j*imsize*2 + i*2 + 1];
-    }
-
-    public FFTArray fft() {
-        // System.out.print("FFTArray: Doing NEW forward transform... ");
-        float[] fft = Arrays.copyOf(data, data.length);
-        FloatFFT_2D f = new FloatFFT_2D(imsize, imsize);
-        f.complexForward(fft);
-        return new FFTArray(imsize, fft);
-    }
-
-    public FFTArray invfft() {
-        float[] dat2 = new float[data.length];
-        for (int i = 0; i < dat2.length; i++) {
-            dat2[i] = data[i]/imsize/imsize;
-        }
-        FloatFFT_2D f = new FloatFFT_2D(imsize, imsize);
-        f.complexInverse(dat2, false);
-        return new FFTArray(imsize, dat2);
-    }
-
-    public FFTArray multiply(SquareArray covArray) {
-        float[] cov = covArray.data;
-        float[] fft2 = new float[2*imsize*imsize];
-        for (int y = 0; y < imsize; y++) {
-            for (int x = 0; x < imsize; x++) {
-                int x1, y1;
-                x1 = x - imsize/2; 
-                y1 = y - imsize/2;
-                if (x1 < 0) x1 += imsize;
-                if (y1 < 0) y1 += imsize;
-                fft2[y1*imsize*2 + x1*2] =  
-                    data[y1*imsize*2 + x1*2] * cov[y*imsize+x];  // Real
-                fft2[y1*imsize*2 + x1*2 + 1] = 
-                    data[y1*imsize*2 + x1*2 + 1]*cov[y*imsize+x];  // Imaginary
-            }
-        }
-        FFTArray fftconv = new FFTArray(imsize, fft2);
-        return fftconv;
-    }    
-}
 
 class SquareArray {
     int size;
@@ -119,8 +44,222 @@ class SquareArray {
     public void set(int i, int j, float val) {
         data[j*size+i] = val;
     }
+
     public float get(int i, int j) {
         return data[j*size+i];
+    }
+
+    float[] minmax() {
+        float min = data[0];
+        float max = data[0];
+        for (int i=1; i < data.length; i++) {
+            if (data[i] < min) min = data[i];
+            if (data[i] > max) max = data[i];
+        }
+        float[] res = {min, max};
+        return res;
+    }
+
+    void scale() {
+        float[] minmaxv = minmax();
+        float min = minmaxv[0];
+        float max = minmaxv[1];
+        int mean=0;
+        for (int i = 0; i < data.length; i++) mean += data[i];
+        // System.out.print("Limits = ["+min+","+max+"], sum = "+mean);
+        mean /= (float) data.length;
+        // System.out.print(", mean = "+mean);
+        for (int i = 0; i < data.length; i++) {
+            data[i] = (data[i] - mean) / (max - min);
+        }
+    }
+
+    public PixArray toPix() {
+        // System.out.println("Converting (datToPix)");
+
+        // determine the scale of the data
+        float[] minmaxv = vriUtils.minmax(data);
+        float min = minmaxv[0];
+        float max = minmaxv[1];
+
+        PixArray pix = new PixArray(size, size);
+        for (int y = 0; y < size; y++) {
+            for  (int x = 0; x < size; x++) {
+                int x1, y1;
+                x1 = x; y1 = y;
+                int grey = (int) (255 * (get(x1, y1) - min) / (max - min));
+                pix.set(x, y, 0xff000000 | (grey << 16) | (grey << 8) | grey);
+            }
+        }
+        return pix;
+    }
+
+
+
+
+}
+
+class FFTArray {
+    int size;
+    float data[];
+
+    public FFTArray(int i) {
+        size = i;
+        data = new float[2*size*size];
+    }
+
+    static FFTArray fromSquareArray(SquareArray sa) {
+        FFTArray result = new FFTArray(sa.size);
+        for (int i=0; i<sa.size; i++) {
+            for (int j=0; j<sa.size; j++) {
+                result.set(i, j, sa.get(i, j), 0);
+            }
+        }
+        return result;
+    }
+
+    public FFTArray(int i, float[] d) {
+        size = i;
+        data = d;
+    }
+
+    public void set(int i, int j, float real, float im) {
+        data[j*size*2 + i*2] = real;
+        data[j*size*2 + i*2 + 1] = im;
+    }
+
+    public float getReal(int i, int j) {
+        return data[j*size*2 + i*2];
+    }
+    
+    public float getImag(int i, int j) {
+        return data[j*size*2 + i*2 + 1];
+    }
+
+    public float getPhase(int i, int j) {
+        return (float) Math.atan2(data[j*size*2+i*2],data[j*size*2+i*2+1]);
+    }
+
+    public float getAmpl(int i, int j) {
+        return (float) Math.sqrt(data[j*size*2+i*2] * data[j*size*2+i*2] +
+                                 data[j*size*2+i*2+1] * data[j*size*2+i*2+1]);
+    }
+
+    public FFTArray fft() {
+        // System.out.print("FFTArray: Doing NEW forward transform... ");
+        float[] fft = Arrays.copyOf(data, data.length);
+        FloatFFT_2D f = new FloatFFT_2D(size, size);
+        f.complexForward(fft);
+        return new FFTArray(size, fft);
+    }
+
+    public FFTArray invfft() {
+        float[] dat2 = new float[data.length];
+        for (int i = 0; i < dat2.length; i++) {
+            dat2[i] = data[i]/size/size;
+        }
+        FloatFFT_2D f = new FloatFFT_2D(size, size);
+        f.complexInverse(dat2, false);
+        return new FFTArray(size, dat2);
+    }
+
+    public FFTArray multiply(SquareArray covArray) {
+        float[] cov = covArray.data;
+        float[] fft2 = new float[2*size*size];
+        for (int y = 0; y < size; y++) {
+            for (int x = 0; x < size; x++) {
+                int x1, y1;
+                x1 = x - size/2; 
+                y1 = y - size/2;
+                if (x1 < 0) x1 += size;
+                if (y1 < 0) y1 += size;
+                fft2[y1*size*2 + x1*2] =  
+                    data[y1*size*2 + x1*2] * cov[y*size+x];  // Real
+                fft2[y1*size*2 + x1*2 + 1] = 
+                    data[y1*size*2 + x1*2 + 1]*cov[y*size+x];  // Imaginary
+            }
+        }
+        FFTArray fftconv = new FFTArray(size, fft2);
+        return fftconv;
+    }    
+
+    public SquareArray extractReals() {
+        SquareArray sa = new SquareArray(size);
+        for (int i=0; i < size; i++) {
+            for (int j=0; j<size; j++) {
+                sa.set(i, j, getReal(i,j));
+            }
+        }
+        return sa;
+    }
+}
+
+
+class PixArray {
+    int height, width;
+    int[] data;
+    
+    public PixArray(int i, int j) {
+        width=i;
+        height=j;
+        data = new int[width*height];
+    }
+
+    public void set(int i, int j, int val) {
+        data[j*width+i] = val;
+    }
+
+    public int get(int i, int j) {
+        return data[j*width+i];
+    }
+
+    public Image toImage(Applet applet, int size) {
+        return applet.createImage(new MemoryImageSource(width, height, data, 0, size));
+    }
+
+    public float meanEdge() {
+        float mean = 0;
+        int count = 0;
+        for (int h = 0; h < height; h++) {
+            int inc = width-1;
+            if (h == 0 || h == (height-1)) inc = 1;
+            for (int w = 0; w < width; w += inc) {
+                mean += (float)(get(w, h) & 0x000000ff);  
+                // Assume already greyscale -
+                // i.e. red = green = blue
+                count++;                                       
+            }
+        }
+        mean /= (float)count;
+        return mean;
+    }
+
+    public SquareArray toDat() {
+        // System.out.println("Converting (PixArray::toDat): "+imh+","+imw+","+size);
+        int size = vriUtils.getImsize(height, width);
+        // find mean value along the edge (to make the padding realistic)
+        float mean = meanEdge();
+        SquareArray dat = new SquareArray(size);
+        for (int h = 0; h < height; h++) {
+            for (int w = 0; w < width; w++) {
+                // Re-centre image.
+                int i = (size/2 + w - width/2);
+                int j = (size/2 + h - height/2);
+                dat.set(i, j, (get(w, h) & 0x000000ff)-mean);
+            }
+        }
+        return dat;
+    }
+
+    public void makeGrey() {
+        // System.out.println("PixArray::greyPix: "+height+"x"+width);
+        for (int i=0; i<height*width; i++) {
+            int red   = (data[i] & 0x00ff0000) >> 16;
+            int green = (data[i] & 0x0000ff00 >> 8);
+            int blue  = (data[i] & 0x000000ff);
+            int grey  = (red+green+blue)/3;
+            data[i] = (data[i] & 0xff000000) | (grey << 16) | (grey << 8) | grey;
+        }
     }
 }
 
@@ -140,25 +279,6 @@ class vriUtils {
         return res;
     }
 
-
-    static void scaleDat(float dat[]) {
-        float[] minmaxv = vriUtils.minmax(dat);
-        float min = minmaxv[0];
-        float max = minmaxv[1];
-        int mean=0;
-        for (int i = 0; i < dat.length; i++) mean += dat[i];
-        // System.out.print("Limits = ["+min+","+max+"], sum = "+mean);
-        mean /= (float) dat.length;
-        // System.out.print(", mean = "+mean);
-        for (int i = 0; i < dat.length; i++) {
-            dat[i] = (dat[i] - mean) / (max - min);
-        }
-        minmaxv = vriUtils.minmax(dat);
-        min = minmaxv[0];
-        max = minmaxv[1];
-        // System.out.println(", new limits = ["+min+","+max+"]");
-    }
-
     static public int getImsize(int imh, int imw) {
 
         // Determine minimum sized box that image could fit into
@@ -173,121 +293,51 @@ class vriUtils {
         return imsize;
     }
 
-    static int[] datToPix(float dat[], int imsize) {
-        // System.out.println("Converting (datToPix)");
 
-        // determine the scale of the data
-        float[] minmaxv = minmax(dat);
-        float min = minmaxv[0];
-        float max = minmaxv[1];
-
-        int pix[] = new int[imsize * imsize];
-        for (int y = 0; y < imsize; y++) {
-            for  (int x = 0; x < imsize; x++) {
-                int x1,y1;
-                x1 = x; y1 = y;
-                int grey = (int) (255 * (dat[y1*imsize*2+x1*2] - min) /
-                                  (max - min));
-                pix[y*imsize+x] = 0xff000000 | (grey << 16) | (grey << 8) | grey;
-            }
-        }
-        return pix;
-    }
-
-    static public void greyPix(int[] pix, int imh, int imw) {
-        // System.out.println("greyPix: "+imh+"x"+imw);
-        if (pix != null) {
-            for (int i=0; i<imh*imw; i++) {
-                int red   = (pix[i] & 0x00ff0000) >> 16;
-                int green = (pix[i] & 0x0000ff00 >> 8);
-                int blue  = (pix[i] & 0x000000ff);
-                int grey  = (red+green+blue)/3;
-                pix[i] = (pix[i] & 0xff000000) |
-                    (grey << 16) | (grey << 8) | grey;
-            }
-        } else {
-            System.err.println("greyPix: pix empty");
-        }    
-    }
-
-    static public float[] pixToDat(int[] pix, int imh, int imw, int imsize) {
-        // System.out.println("Converting (pixToDat): "+imh+","+imw+","+imsize);
-        if (pix == null) {
-            System.err.println("pixToDat: pix empty");
-            return new float[]{(float)0.0};
-        }
-
-        // find mean value along the edge (to make the padding realistic)
-        float mean = 0;
-        int count = 0;
-        for (int h = 0; h < imh; h++) {
-            int inc = imw-1;
-            if (h == 0 || h == (imh-1)) inc = 1;
-            for (int w = 0; w < imw; w += inc) {
-                mean += (float)(pix[h*imw + w] & 0x000000ff);  
-                // Assume already greyscale -
-                // i.e. red = green = blue
-                count++;                                       
-            }
-        }
-        mean /= (float)count;
-        float dat[] = new float[imsize*2*imsize];
-        for (int h = 0; h < imh; h++) {
-            for (int w = 0; w < imw; w++) {
-                dat[(imsize/2 + h - imh/2)*imsize*2 + (imsize/2+w-imw/2)*2] = 
-                    (pix[h*imw + w] & 0x000000ff)-mean;
-            }
-        }
-        // scale to range of one around zero
-        return dat;
-    }
-
-    static int[] fftToPix(float fft[], vriGreyDisp.Types type, int imsize) {
-        double value;        // Quantity that is plotted to a pixel
-        // System.out.println("Converting (fftToPix)");
-
+    static PixArray fftToPix(FFTArray fft, PlotTypes type) {
+        float value;        // Quantity that is plotted to a pixel
         // determine the scale of the data (use a forced -180:180 range for phase
-        float[] minmaxv = minmax(fft);
+        int size = fft.size;
+        float[] minmaxv = vriUtils.minmax(fft.data);
         float min = minmaxv[0];
         float max = minmaxv[1];
-        if (type == vriGreyDisp.Types.PHASE) {
+        if (type == PlotTypes.PHASE) {
             min = (float)(-Math.PI / 2.0);
             max = (float)( Math.PI / 2.0);
         }
 
-        int pix[] = new int[imsize * imsize];
-        for (int y = 0; y < imsize; y++) {
-            for  (int x = 0; x < imsize; x++) {
+        PixArray pix = new PixArray(size, size);
+        for (int y = 0; y < size; y++) {
+            for  (int x = 0; x < size; x++) {
                 int x1, y1;
                 // shift origin to center of image
-                x1 = x - imsize/2;
-                y1 = y - imsize/2;
-                if (x1 < 0) x1 += imsize;
-                if (y1 < 0) y1 += imsize;
+                x1 = (x + size/2) % size;
+                y1 = (y + size/2) % size;
 
                 // Depending on the "type" of display selected, we extract the 
                 // relevant components of the fourier transform.
-                if (type == vriGreyDisp.Types.REAL) {
-                    value = fft[y1*imsize*2+x1*2];
-                } else if (type == vriGreyDisp.Types.IMAG) {
-                    value = fft[y1*imsize*2+x1*2+1];
-                } else if (type == vriGreyDisp.Types.PHASE) {
-                    value = Math.atan2(fft[y1*imsize*2+x1*2],fft[y1*imsize*2+x1*2+1]);
-                } else {
-                    // Used for Ampl., Colour and others that aren't handled
-                    value = Math.sqrt(fft[y1*imsize*2+x1*2]*fft[y1*imsize*2+x1*2] +
-                                      fft[y1*imsize*2+x1*2+1]*fft[y1*imsize*2+x1*2+1]);
+                switch (type) {
+                case REAL: 
+                    value = fft.getReal(x1, y1);
+                    break;
+                case IMAG:
+                    value = fft.getImag(x1, y1);
+                    break;
+                case PHASE:
+                    value = fft.getPhase(x1, y1); 
+                    break;
+                default:  // Used for Ampl., Colour and others that aren't handled
+                    value = fft.getAmpl(x1, y1);
                 }
 
                 int grey = (int) (255.0 * (value - min) / (max - min) );
-                pix[y*imsize+x] = 0xff000000 | (grey << 16) | (grey << 8) | grey;
+                pix.set(x, y, 0xff000000 | (grey << 16) | (grey << 8) | grey);
 
-                if(type.equals("Colour")) {
-                    double h = Math.atan2(fft[y1*imsize*2+x1*2],fft[y1*imsize*2+x1*2+1])
-                        / Math.PI / 2.0;
-                    if(h < 0.0) h += 1.0;
-                    pix[y*imsize+x] = Color.HSBtoRGB( (float) h, (float) 1.0,
-                                                      (float) grey/(float)255.0);
+                if (type.equals("Colour")) {
+                    double h = fft.getPhase(x1, y1) / Math.PI / 2.0;
+                    if (h < 0.0) h += 1.0;
+                    pix.set(x, y, Color.HSBtoRGB( (float) h, (float) 1.0,
+                                                  (float) grey/(float)255.0));
                 }  // End if(colour)
             }  // End for(x)
         }  // End for(y)
@@ -296,14 +346,13 @@ class vriUtils {
 }
 
 class vriGreyDisp extends vriDisplay {
-    public enum Types {PHASE, REAL, IMAG, AMPL, COLOUR};
     Applet applet;
     //  boolean replot = false;
     URL imgURL;
     Image img;
     static int imsize;     // Size of the "squared" image
     String message = null; // Message to print on the Display
-    Types type = Types.AMPL;    // Used to select real/imag/amp/phase display
+    PlotTypes type = PlotTypes.AMPL;    // Used to select real/imag/amp/phase display
     double fullScale = 73000.0;
     String unit = "lobster";
     boolean hasAxes = false;
@@ -431,34 +480,23 @@ class vriGreyDisp extends vriDisplay {
         }
     }
 
-    public int[] imgToPix(Image img) 
+    public PixArray imgToPix(Image img) 
         throws EmptyImageException
     {
-        // System.out.println("Converting (imgToPix)");
-
         if (img == null) {
             throw new EmptyImageException();
         } 
         int imh = img.getHeight(this);
         int imw = img.getWidth(this);
         // System.out.println("Got "+this+"image: size = "+imw+"x"+imh);
-        int pix[] = new int[imh*imw];
+        PixArray pix = new PixArray(imw, imh);
         try {
-            PixelGrabber pg = new PixelGrabber(img, 0, 0, imw, imh, pix, 0, imw);
+            PixelGrabber pg = new PixelGrabber(img, 0, 0, imw, imh, pix.data, 0, imw);
             pg.grabPixels(100);
         } catch (InterruptedException e){
             System.err.println("imgToPix failed");
         }
         return pix;
     }
-
-    public void pixToImg(int pix[], int imsize) {
-        //System.out.println("Converting (pixToImg): "+
-        // imsize+"x"+imsize);
-
-        img = applet.createImage(new MemoryImageSource(imsize, imsize, pix, 0, imsize));
-        repaint();
-    }
-
 }
 
